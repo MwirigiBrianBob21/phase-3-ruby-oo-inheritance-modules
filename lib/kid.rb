6eb3ede7caa0a8f5df4004ece3d6cce31a3b3dba
MwirigiBrianBob21 <brian.mwirigi@students.moringaschool.com>
2022-10-25 15:22:58 +0300

    Complete

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,21 @@
 require_relative './dance_module'
 require_relative './meta_dancing_module'
+require_relative './fancy_dance'
+
+class Kid
+    include Dance
+    extend MetaDancing
+    # :: references the parent and child relationship of the nested modules
+    extend FancyDance::ClassMethods
+    include FancyDance::InstanceMethods 
+
+attr_accessor :name
+
+    def initialize(name)
+        @name = name
+    end
+end
+
+buster = Kid.new("angelinas")
+puts buster.take_a_bow
+puts buster.jump
